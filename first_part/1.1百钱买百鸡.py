f271e53f2bf78416d980609cb0d881d40656579c
micah <844830117@qq.com>
2023-04-14 17:49:53 +0800

    # test

--- first_part/1.1百钱买百鸡.py
+++ first_part/1.1百钱买百鸡.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 # @author  : micah
-# @time    : 2023/3/17 14:41
+# @time    : 2023/4/17 14:41
 # @function: 公鸡5块，母鸡3块，雏鸡1块3只。用100块买100只
-# @version :
+# @version :  4.14测试
 
 for i in range(1, 20):
     for j in range(1, 33):
